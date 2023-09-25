24a0b38f1cae915756b7aab902207085150e4d2d
Izet Delibasic <izetdelibasic55@gmail.com>
2023-09-26 01:42:54 +0200

    Adding some new assets

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    public bool playerinRange;
+
+
     public string ItemName;
 
     public string GetItemName()
     {
         return ItemName;
     }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && playerinRange && SelectionManager.Instance.onTarget){
+            Debug.Log("Item added to inventory");
+
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerinRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerinRange = false;
+        }
+    }
+
 }
