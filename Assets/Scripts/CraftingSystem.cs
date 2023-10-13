0d9d5a76f8e55c795ad1c90ef6d9180414f45b59
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-13 18:05:34 +0200

    Fixing scripts for crafting, adding new cursor style

--- Assets/Scripts/CraftingSystem.cs
+++ Assets/Scripts/CraftingSystem.cs
@@ -69,7 +69,7 @@ public class CraftingSystem : MonoBehaviour
 
     void CraftAnyItem(Blueprint blueprintToCraft)
     {
-        InventorySystem.Instance.AddToInventory(AxeBLP.itemName);
+        InventorySystem.Instance.AddToInventory(blueprintToCraft.itemName);
 
         if(blueprintToCraft.numOfRequirements == 1)
         {
@@ -82,15 +82,25 @@ public class CraftingSystem : MonoBehaviour
 
         }
 
-        InventorySystem.Instance.ReCalculateList();
+        StartCoroutine(calculate());
 
         RefreshNeededItems();
+    }
+
+    public IEnumerator calculate()
+    {
+        yield return new WaitForSeconds(1f);
+
+        InventorySystem.Instance.ReCalculateList();
     }
 
 
     // Update is called once per frame
     void Update()
     {
+
+        RefreshNeededItems();
+
         if (Input.GetKeyDown(KeyCode.C) && !isOpen)
         {
             craftingScreenUI.SetActive(true);
@@ -111,7 +121,7 @@ public class CraftingSystem : MonoBehaviour
 
 
     }
-        private void RefreshNeededItems(){
+     private void RefreshNeededItems(){
             int stone_count = 0;
             int stick_count = 0;
 
@@ -130,19 +140,20 @@ public class CraftingSystem : MonoBehaviour
                 }
             }
 
-            // AXE
 
-            AxeReq1.text = "3 Stone (" + stone_count + ")";
-            AxeReq2.text = "3 Stick (" + stick_count + ")";
+        // AXE
 
-            if(stone_count >= 3 && stick_count >= 3)
-            {
-                craftAxeBTN.gameObject.SetActive(true);
-            }
-            else
-            {
-                craftAxeBTN.gameObject.SetActive(false);
-            }
+        AxeReq1.text = "3 Stone [" + stone_count + "]";
+        AxeReq2.text = "3 Stick [" + stick_count + "]";
 
+        if (stone_count >= 3 && stick_count >= 3)
+        {
+            craftAxeBTN.interactable = true;
         }
+        else
+        {
+            craftAxeBTN.interactable = false;
+        }
+
+    }
 }
